49b97d3bb6f5d4ef156761613ff0f6196b01c367
Katy <kuerten193@gmail.com>
2020-07-26 23:49:34 +1000

    change for production

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	publicRuntimeConfig: {
+		APP_NAME: 'NODE-REACT-AWS',
+		API: 'https://hacker-193-api.herokuapp.com/api',
+		PRODUCTION: true,
+		DOMAIN: 'https://hacker-193.herokuapp.com',
+		FB_APP_ID: 'IFDJFEIFNVIE'
+	}
+};
